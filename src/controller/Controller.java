3acc527fd9f5d371b8787a532054af37cbfe2940
Ali <ali138210@yahoo.com>
2023-06-27 11:55:35 +0330

    enahnced

--- src/controller/Controller.java
+++ src/controller/Controller.java
@@ -17,23 +17,39 @@ public class Controller {
         //User:.....,,Pass:.....
         ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(Paths.get("src/Data/User&Pass.txt"));
         for (String str : lines)
-            if (str.equals(("User:" + data.get("User") + ",,Pass:" + data.get("Pass"))))
+            if (str.equals(("User:" + data.get("User") + ",,Pass:" + data.get("Pass")))) {
+                ArrayList<String> line = new ArrayList<>();
+                line.add(data.get("User"));
+                Files.write(Paths.get("src/Data/CurrentUser.txt"), line);
                 return "logged in successfully";
+            }
         return "Username or Password is wrong";
 
     }
 
+    private boolean conditions(String str, int n) {
+        for (int i = 0; i < n; i++) {
+
+            if ((str.charAt(i) >= '0' || str.charAt(i) <= '9') && (str.charAt(i) >= 'A' || str.charAt(i) <= 'Z')) {
+                return true;
+            }
+        }
+        return false;
+    }
+
     private String register(HashMap<String, String> data) {
         //register
-        //User:....,,Pass:...
+        //User:....,,Email:......,,Pass:...
         try {
             ArrayList<String> lines = new ArrayList<>(Files.readAllLines(Paths.get("src/Data/User&Pass.txt")));
-            if (lines.contains(Convertor.MapToString(data)))
-                return "account is already created";
-            else {
-                DataBase.getInstance("register").getTable("register").insert(data);
-                return "account successfully created";
+            for (String str : lines) {
+                if (str.contains(data.get("User")))
+                    return "account is already created";
             }
+            if (data.get("Pass").length() < 8 || data.get("Pass").contains(data.get("User")) || !data.get("Email").contains("@yahoo.com")  || !this.conditions(data.get("Pass"), data.get("Pass").length()))
+                return "Password or email isn't right!";
+            DataBase.getInstance("register").getTable("register").insert(data);
+            return "account successfully created";
         } catch (IOException e) {
             return "something went wrong";
         }
@@ -44,13 +60,13 @@ public class Controller {
         //Name:....,,Rate:...,,Type:...
         if (data.get("Type").equalsIgnoreCase("AudioBooks")) {
             ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(Paths.get("src/Data/AudioBooks/" + data.get("Name") + "/Info.txt"));
-            lines.set(0, String.valueOf((Integer.valueOf(lines.get(0)) + Integer.valueOf(data.get("Rate")))));
-            lines.set(1, "Count:" + (Integer.valueOf(lines.get(1).split(":")[1]) + 1));
+            lines.set(0, String.valueOf((Integer.parseInt(lines.get(0)) + Integer.parseInt(data.get("Rate")))));
+            lines.set(1, "Count:" + (Integer.parseInt(lines.get(1).split(":")[1]) + 1));
             Files.write(Paths.get("src/Data/AudioBooks/" + data.get("Name") + "/Info.txt"), lines);
         } else {
             ArrayList<String> lines = (ArrayList<String>) Files.readAllLines(Paths.get("src/Data/Books/" + data.get("Name") + "/Info.txt"));
-            lines.set(0, String.valueOf((Integer.valueOf(lines.get(0)) + Integer.valueOf(data.get("Rate")))));
-            lines.set(1, "Count:" + (Integer.valueOf(lines.get(1).split(":")[1]) + 1));
+            lines.set(0, String.valueOf((Integer.parseInt(lines.get(0)) + Integer.parseInt(data.get("Rate")))));
+            lines.set(1, "Count:" + (Integer.parseInt(lines.get(1).split(":")[1]) + 1));
             Files.write(Paths.get("src/Data/Books/" + data.get("Name") + "/Info.txt"), lines);
         }
         return "rate successfully added";
@@ -62,7 +78,7 @@ public class Controller {
         ArrayList<String> fileContent = new ArrayList<>(Files.readAllLines(Paths.get("src/Data/AccountsNetWorth.txt")));
         for (int i = 0; i < fileContent.size(); i++) {
             if (fileContent.get(i).contains(data.get("User"))) {
-                String newLine = "Money:" + (Integer.valueOf(data.get("Money")) + Integer.valueOf(fileContent.get(i).split(",,")[0].split(":")[1])) + ",," + "User:" + data.get("User");
+                String newLine = "Money:" + (Integer.parseInt(data.get("Money")) + Integer.parseInt(fileContent.get(i).split(",,")[0].split(":")[1])) + ",," + "User:" + data.get("User");
                 fileContent.set(i, newLine);
                 Files.write(Paths.get("src/Data/AccountsNetWorth.txt"), fileContent);
                 return "Money added successfully";
--- src/network/ClientHandler.java
+++ src/network/ClientHandler.java
@@ -21,16 +21,59 @@ public class ClientHandler extends Thread {
         try {
             DataInputStream dis = new DataInputStream(socket.getInputStream());
             DataOutputStream dos = new DataOutputStream(socket.getOutputStream());
-            String req = dis.readUTF();
-            Scanner scanner = new Scanner(req);
-            // command register
-            //data:ali,,iranmanesh
+            StringBuilder req = new StringBuilder();
+            int c = dis.read();
+            while (c != 0) {
+                req.append((char) c);
+                c = dis.read();
+            }
+            Scanner scanner = new Scanner(req.toString());
             String command = scanner.nextLine();
-            String data = scanner.nextLine();
+            String initdata = scanner.nextLine();
+            String[] inits = initdata.split(",,");
+            String data = "";
+            String start = "â\u0094¤";
+            String end = "â\u0094\u009C";
+            int startIndex = inits[0].indexOf(start);
+            int endIndex = inits[0].indexOf(end, startIndex + start.length());
+            String user = (inits[0].substring(startIndex + start.length(), endIndex));
+            int startIndexp = inits[1].indexOf(start);
+
+            switch (command) {
+                case "Authorize" -> {
+                    int endIndexp = inits[1].indexOf(end, startIndexp + start.length());
+                    String pass = (inits[1].substring(startIndexp + start.length(), endIndexp));
+                    data = "User:" + user + ",,Pass:" + pass;
+                }
+                case "addToWallet" -> {
+                    int endIndexp = inits[1].indexOf(end, startIndexp + start.length());
+                    String pass = (inits[1].substring(startIndexp + start.length(), endIndexp));
+                    data = "User:" + user + ",,Money:" + pass;
+                }
+                case "RateBook" -> {
+                    String Name = (inits[0].substring(startIndex + start.length(), endIndex));
+                    int startIndexppp = inits[1].indexOf(start);
+                    int endIndexp = inits[1].indexOf(end, startIndexppp + start.length());
+                    String Rate = (inits[1].substring(startIndexp + start.length(), endIndexp));
+                    int startIndexpp = inits[2].indexOf(start);
+                    int endIndexpp = inits[2].indexOf(end, startIndexpp + start.length());
+                    String Type = (inits[2].substring(startIndexpp + start.length(), endIndexpp));
+                    data = "Name:" + Name + ",,Rate:" + Rate + ",,Type:" + Type;
+                }
+                case "register" -> {
+                    int endIndexppp = inits[1].indexOf(end, startIndexp + start.length());
+                    String pass = (inits[1].substring(startIndexp + start.length(), endIndexppp));
+                    int endIndexpp = inits[2].indexOf(end, startIndexp + start.length());
+                    String email = (inits[2].substring(startIndexp + start.length()+1, endIndexpp));
+                    data = "User:" + user + ",,Pass:" + pass + ",,Email:" + email;
+                }
+            }
+            // command register
+            //data:ali,,iranmanesh,,email
 
             String res = new Controller().run(command, data);
 
-            dos.writeUTF(res);
+            dos.writeBytes(res);
             dos.flush();
 
             dos.close();
